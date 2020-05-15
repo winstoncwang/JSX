be2fe02545b890e2ba452c1240f303b90988c0aa
winstoncwang <congwang@live.co.uk>
2020-05-15 21:06:03 +0100

    comment template naive method with faker.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,30 @@
 // import react and react dom libraries
 import React from 'react';
 import ReactDOM from 'react-dom';
+import faker from 'faker';
 
 // create a react components
 const App = () => {
-	return <div>hi there!</div>; //jsx syntax
+	return (
+		<div className="ui container comments">
+			<div className="comment">
+				<a href="/" className="avatar">
+					<img alt="avatar" src={faker.image.avatar()} />
+				</a>
+				<div className="content">
+					<a href="/" className="author">
+						Sam
+					</a>
+					<div className="metadata">
+						<div className="date">Today 16:50</div>
+					</div>
+					<div className="text">
+						Hey guys, I hope this example comment helpful.
+					</div>
+				</div>
+			</div>
+		</div>
+	); //jsx syntax
 };
 
 // take react components and show it on the screen
