1e13ab889f646ed99ce82c925110a3da876a35c3
winstoncwang <congwang@live.co.uk>
2020-05-15 20:42:58 +0100

    styling library semantics ui

--- src/index.js
+++ src/index.js
@@ -4,17 +4,7 @@ import ReactDOM from 'react-dom';
 
 // create a react components
 const App = () => {
-	return (
-		<div>
-			<label class="label" for="name">
-				Enter name:
-			</label>
-			<input id="name" type="text" />
-			<button style={{ backgroundColor: 'blue', color: 'white' }}>
-				Submit
-			</button>
-		</div>
-	); //jsx syntax
+	return <div>hi there!</div>; //jsx syntax
 };
 
 // take react components and show it on the screen
