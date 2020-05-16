8ffc6ceaf4d642b75cbc13bc73fea38ebe667ba9
winstoncwang <congwang@live.co.uk>
2020-05-16 15:06:09 +0100

    adding an approval reject card

--- src/index.js
+++ src/index.js
@@ -3,11 +3,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import faker from 'faker';
 import CommentDetail from './CommentDetail';
+import ApprovalCard from './ApprovalCard';
 
 // create a react components
 const App = () => {
 	return (
 		<div className="ui container comments">
+			<ApprovalCard />
 			<CommentDetail
 				author="Sam"
 				timeAgo="Today at 4pm"
