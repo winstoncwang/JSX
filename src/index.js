ca2b261ba6f3d5507afea65d40022aefd4fd8881
winstoncwang <congwang@live.co.uk>
2020-05-16 15:12:38 +0100

    Adding props.children for components to be resusable

--- src/index.js
+++ src/index.js
@@ -9,13 +9,14 @@ import ApprovalCard from './ApprovalCard';
 const App = () => {
 	return (
 		<div className="ui container comments">
-			<ApprovalCard />
-			<CommentDetail
-				author="Sam"
-				timeAgo="Today at 4pm"
-				comment="Hello mate"
-				avatar={faker.image.avatar()}
-			/>
+			<ApprovalCard>
+				<CommentDetail
+					author="Sam"
+					timeAgo="Today at 4pm"
+					comment="Hello mate"
+					avatar={faker.image.avatar()}
+				/>
+			</ApprovalCard>
 			<CommentDetail
 				author="Alex"
 				timeAgo="Today at 6pm"
